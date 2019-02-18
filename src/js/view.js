b86c622552bf82a4503d912977ec268721949527
Camila Lima Martins <camilalm31@gmail.com>
2019-02-18 14:44:12 -0300

    hover pagina atual

--- src/js/view.js
+++ src/js/view.js
@@ -1,15 +1,16 @@
-import{montarPaginacao,render,removeContactsList,removePageList} from '../index.js'
+import { montarPaginacao, render, removeContactsList, removePageList } from '../index.js'
+import { isNullOrUndefined } from 'util';
 //botao novo
 let titulo = document.getElementById('new-title');
 let modal = document.getElementById('modal-add-edit');
 let btnNew = document.getElementById('btn-new');
 let span = document.getElementsByClassName('close')[0];
-btnNew.onclick = ()=>{
+btnNew.onclick = () => {
     modal.style.display = 'block';
     titulo.innerHTML = 'Novo Contato';
     document.getElementById('form-new-edit').reset()
 }
-span.onclick = ()=>{
+span.onclick = () => {
     modal.style.display = 'none';
 }
 
@@ -17,67 +18,76 @@ let btnBack = document.getElementById('back')
 let btnForward = document.getElementById('forward')
 
 btnBack.onclick = () => {
-    let  page = 1
-    if(window.state.currentPage != 1){
-        page =window.state.currentPage - 1
-    } 
+    if (!isNullOrUndefined(document.getElementsByClassName('currentPage')[0])) {
+        console.log("funcionou")
+        document.getElementById(window.state.currentPage).classList.remove('currentPage')
+    }
+    let page = 1
+    if (window.state.currentPage != 1) {
+        page = window.state.currentPage - 1
+    }
     window.state = {
         ...window.state,
         currentPage: page
     }
-    console.log(window.state.currentPage)
+
     let control = (window.state.currentPage % 6)
-    
-   
+
     if (control > 0) {
         removeContactsList()
         render()
     } else if (control == 0) {
-        let array = window.state.currentArray - 1  
+        let array = window.state.currentArray - 1
         window.state = {
             ...window.state,
-            currentArray:array
+            currentArray: array
         }
         removeContactsList()
         removePageList()
         montarPaginacao()
-        render() 
+        render()
     }
+    document.getElementById(window.state.currentPage).classList.add('currentPage')
 }
 btnForward.onclick = () => {
-    let  page = window.state.currentPage +1
-    if(page > window.state.contacts.length){
+    if (!isNullOrUndefined(document.getElementsByClassName('currentPage')[0])) {
+        console.log("funcionou")
+        document.getElementById(window.state.currentPage).classList.remove('currentPage')
+    }
+    let page = window.state.currentPage + 1
+    if (page > window.state.contacts.length) {
         page = window.state.contacts.length
     }
     window.state = {
         ...window.state,
         currentPage: page
     }
-   
+
     let control = calculateControl()
-    if (control < 1) {
+    if (control <= 1) {
         removeContactsList()
         render()
-    } else if (control >= 1) {
-        let array = window.state.currentArray + 1 
-      
+    } else if (control > 1) {
+        let array = window.state.currentArray + 1
+
         window.state = {
             ...window.state,
-            currentArray:array
+            currentArray: array
         }
         removeContactsList()
         removePageList()
         montarPaginacao()
-        render() 
+        render()
     }
+    document.getElementById(window.state.currentPage).classList.add('currentPage')
 }
 
-function calculateControl(){
+function calculateControl() {
     let array = 1;
-    if(window.state.currentArray > 0){
-        array=window.state.currentArray +1
+    if (window.state.currentArray > 0) {
+        array = window.state.currentArray + 1
     }
-    return (window.state.currentPage/ array) / 6
+    return (window.state.currentPage / array) / 6
 }
 
 
